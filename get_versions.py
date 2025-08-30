2bbb1113186e2e53d9e8c0117533696a75729eda
AryanGupta1211 <aryangupta112002@gmail.com>
2025-08-30 15:34:32 +0530

    UV project initialisation done

---
+++ get_versions.py
@@ -0,0 +1,17 @@
+import importlib.metadata
+packages = [
+    "langchain",
+    "python-dotenv",
+    "ipykernel",
+    "langchain-core",
+]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
+
+# # serve static & templates
+# app.mount("/static", StaticFiles(directory="../static"), name="static")
+# templates = Jinja2Templates(directory="../templates")
