49964525a0e81e9f9b5c39eed6b30a8c43eda813
AryanGupta1211 <aryangupta112002@gmail.com>
2025-09-27 12:22:43 +0530

    MCP Integration done

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -1,20 +1,14 @@
 import os
 from langchain_astradb import AstraDBVectorStore
-from typing import List
-from langchain_core.documents import Document
 from utils.config_loader import load_config
 from utils.model_loader import ModelLoader
 from dotenv import load_dotenv
-import sys
-from pathlib import Path
-
 from langchain.retrievers.document_compressors import LLMChainFilter, LLMChainExtractor
 from langchain.retrievers import ContextualCompressionRetriever
 from evaluation.ragas_eval import evaluate_context_precision, evaluate_response_relevancy
-
 # Add the project root to the Python path for direct script execution
-project_root = Path(__file__).resolve().parents[2]
-sys.path.insert(0, str(project_root))
+# project_root = Path(__file__).resolve().parents[2]
+# sys.path.insert(0, str(project_root))
 
 class Retriever:
     def __init__(self):
@@ -24,13 +18,13 @@ class Retriever:
         self.config=load_config()
         self._load_env_variables()
         self.vstore = None
-        self.retriever = None
+        self.retriever_instance = None
     
     def _load_env_variables(self):
         """_summary_
         """
         load_dotenv()
-        
+         
         required_vars = ["GOOGLE_API_KEY", "ASTRA_DB_API_ENDPOINT", "ASTRA_DB_APPLICATION_TOKEN", "ASTRA_DB_KEYSPACE"]
         
         missing_vars = [var for var in required_vars if os.getenv(var) is None]
@@ -56,28 +50,28 @@ class Retriever:
                 token=self.db_application_token,
                 namespace=self.db_keyspace,
                 )
-        if not self.retriever:
+        if not self.retriever_instance:
             top_k = self.config["retriever"]["top_k"] if "retriever" in self.config else 3
             
             mmr_retriever=self.vstore.as_retriever(
                 search_type="mmr",
-                search_kwargs={ "k": top_k,
+                search_kwargs={"k": top_k,
                                 "fetch_k": 20,
                                 "lambda_mult": 0.7,
                                 "score_threshold": 0.6
-                                })
+                               })
             print("Retriever loaded successfully.")
             
             llm = self.model_loader.load_llm()
             
             compressor=LLMChainExtractor.from_llm(llm)
             
-            self.retriever = ContextualCompressionRetriever(
+            self.retriever_instance = ContextualCompressionRetriever(
                 base_compressor=compressor, 
                 base_retriever=mmr_retriever
             )
             
-            return self.retriever
+        return self.retriever_instance
             
     def call_retriever(self,query):
         """_summary_
